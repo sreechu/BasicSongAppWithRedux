4b6a307f1e182817daefc24dcc39b4931295d20a
Sree Charan <srees@email.arizona.edu>
2020-10-10 00:26:44 -0700

    temp spotify api2

--- src/components/FetchSongs.js
+++ src/components/FetchSongs.js
@@ -1,20 +1,16 @@
 import React, { useEffect,useState } from 'react';
 import axios from 'axios';
 import './constants';
-import hash from './hash';
 
-  export const FetchSongs = () => {
+  export const FetchSongs = ({token}) => {
 
-   const [token,setToken] = useState(null);
+  
 const [tracks,setTracks] = useState(null);
 
     useEffect(() => {
+       
+        if (token) {
         // Set token
-        let _token = hash.access_token;
-        if (_token) {
-        // Set token
-        setToken(_token);
-
         axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
 
         axios.get('https://api.spotify.com/v1/me/player')
--- src/components/LoginToSpotify.js
+++ src/components/LoginToSpotify.js
@@ -2,27 +2,9 @@ import React, { useState,useEffect } from "react";
 import hash from "./hash";
 import * as Constants from './constants';
 
-
+import FetchSongs from './FetchSongs';
 export const LoginToSpotify = () => {
 const [token,setToken] = useState(null);
-
-
-/**
- * https://accounts.spotify.com/authorize?
- * response_type=code&
- * client_id=77e602fc63fa4b96acff255ed33428d3&
- * redirect_uri=http%3A%2F%2Flocalhost&scope=user-follow-modify&
- * state=e21392da45dbf4&
- * code_challenge=KADwyz1X~HIdcAG20lnXitK6k51xBP4pEMEZHmCneHD1JhrcHjE1P3yU_NjhBz4TdhV6acGo16PCd10xLwMJJ4uCutQZHw&
- * code_challenge_method=S256
- * 
- * 
- * 
- * https://accounts.spotify.com/authorize?
- * client_id=5fe01282e94241328a84e7c5cc169164&redirect_uri=http:%2F%2Fexample.com%2Fcallback
- * &scope=user-read-private%20user-read-email&response_type=token&state=123
- */
-
 
 
 // Similar to componentDidMount and componentDidUpdate:
@@ -35,19 +17,16 @@ useEffect(() => {
        // Set token
        setToken(_token);
      }
-  });
+  },[token]);
     
   let url = 'https://accounts.spotify.com/authorize';
   url += '?response_type=token';
   url += '&client_id=' + encodeURIComponent(Constants.CLIENT_ID);
   url += '&redirect_uri=' + encodeURIComponent('https://stupefied-euler-0f6cd2.netlify.app/');
   url += '&state=' + encodeURIComponent('123');
- 
 
     return (
-    <div >
-      <header className="App-header">
-      <img  className="App-logo" alt="logo" />
+    <div>
       {!token && (
         <a
           className="btn btn--loginApp-link"
@@ -56,15 +35,11 @@ useEffect(() => {
           Login to Spotify
         </a>
       )}
-      </header>
-    </div>
-
+    <div className="fetch-songs">
+      {token && <FetchSongs token={token}/> }
+      </div>
+</div>
     )
-}
+};
+
 export default LoginToSpotify;
-
-/**
-      {token && (
-        // Spotify Player Will Go Here In the Next Step
-      )} */
-
