d422da052cdc5c14979308c7d7098188304dd733
Sree Charan <srees@email.arizona.edu>
2020-10-10 17:08:21 -0700

    simple app

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,19 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="ui container grid grid-margin">
-      
-      
-      SongList:
-      <div className="ui container row">
-      
-        <div className="column eight wide">
-          <SongList />
-         
-        </div> <div className="column eight wide">
-        <SongDetail/></div>
+    <div className="grid-margin">
+      <div className="ui container grid ">
+        Please read about Redux for this use case
+        <a href="https://github.com/sreechu/BasicSongAppWithRedux">here</a>
+        <h2>SongList:</h2>
+        <div className="ui container row">
+          <div className="column eight wide">
+            <SongList />
+          </div>{" "}
+          <div className="column eight wide">
+            <SongDetail />
+          </div>
+        </div>
       </div>
     </div>
   );
